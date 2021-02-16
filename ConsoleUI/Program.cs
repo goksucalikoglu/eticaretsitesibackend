67a8c0e581d2d50d11994e951d4197e1170bca35
goksucalikoglu <fatmagoksuc@gmail.com>
2021-02-16 21:09:28 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result =productManager.GetProductDetails();
+            
+                if(result.Success==true)
+                {
+                    foreach(var product in result.Data)
+                    {
+                        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(result.Message);
+                }
+
+            
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+
+      
+
+    }
+}
